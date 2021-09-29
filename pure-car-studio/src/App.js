ee5305ec5dbb6be8c0de330225a6e941fac5ef4e
ossowskid <ossowski.dawid@gmail.com>
2021-09-29 19:37:47 +0200

    wip

--- pure-car-studio/src/App.js
+++ pure-car-studio/src/App.js
@@ -1,6 +1,4 @@
-import logo from './logo.svg';
 import './App.css';
-import { Header } from './Styled/header.styled';
 import { HeaderMenu } from './Components/Header';
 
 function App() {
--- pure-car-studio/src/Styled/header.styled.js
+++ pure-car-studio/src/Styled/header.styled.js
@@ -2,9 +2,11 @@ import styled from "styled-components";
 
 export const Header = styled.header`
 display: flex;
+position: sticky;
 width: 100vw;
 height: 90px;
 background-color: #111111;
+border-bottom: 1px solid #ffc8a3;
 `;
 export const Logo = styled.div`
 width: 50vw`;
@@ -13,10 +15,15 @@ width: 50vw;
 display: flex;
 align-items: center;
 justify-content: space-around;
-background-color: tomato;
 list-style: none;
 `;
 export const NavigationMenu = styled.li`
 display: flex;
 color: white;
+font-size: 20px;
+cursor: pointer;
+&:hover {
+    color: #ffc8a3;
+    font-size: 20px;
+}
 `;

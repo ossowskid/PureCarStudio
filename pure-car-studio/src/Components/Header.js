83070e11d0411ba45690065acacdd317e4793258
ossowskid <ossowski.dawid@gmail.com>
2021-11-30 18:50:21 +0100

    adding logo image

--- pure-car-studio/src/Components/Header.js
+++ pure-car-studio/src/Components/Header.js
@@ -1,21 +1,30 @@
 import React from "react";
-import { Header, Navigation, NavigationMenu, Logo } from "../Styled/header.styled";
+import {
+  Header,
+  Navigation,
+  NavigationMenu,
+  Logo,
+} from "../Styled/header.styled";
 
 export const HeaderMenu = () => {
-
-    return (
-        <>
-            <Header>
-                <Logo></Logo>
-                <Navigation>
-                    <NavigationMenu>Home</NavigationMenu>
-                    <NavigationMenu>Oferta</NavigationMenu>
-                    <NavigationMenu>Cennik</NavigationMenu>
-                    <NavigationMenu>Realizacje</NavigationMenu>
-                    <NavigationMenu>O firmie</NavigationMenu>
-                    <NavigationMenu>Kontakt</NavigationMenu>
-                </Navigation>
-            </Header>
-        </>
-    )
-}
+  return (
+    <>
+      <Header>
+        <Logo>
+          <img
+            src={require("../images/Logo200H.png")}
+            alt="Pure Car Studio logo"
+          />
+        </Logo>
+        <Navigation>
+          <NavigationMenu>Home</NavigationMenu>
+          <NavigationMenu>Oferta</NavigationMenu>
+          <NavigationMenu>Cennik</NavigationMenu>
+          <NavigationMenu>Realizacje</NavigationMenu>
+          <NavigationMenu>O firmie</NavigationMenu>
+          <NavigationMenu>Kontakt</NavigationMenu>
+        </Navigation>
+      </Header>
+    </>
+  );
+};
--- pure-car-studio/src/Styled/header.styled.js
+++ pure-car-studio/src/Styled/header.styled.js
@@ -1,29 +1,30 @@
 import styled from "styled-components";
 
 export const Header = styled.header`
-display: flex;
-position: sticky;
-width: 100vw;
-height: 90px;
-background-color: #111111;
-border-bottom: 1px solid #ffc8a3;
+  display: flex;
+  position: sticky;
+  width: 100vw;
+  height: 90px;
+  background-color: #111111;
+  border-bottom: 1px solid #ffc8a3;
 `;
 export const Logo = styled.div`
-width: 50vw`;
+  width: 50vw;
+`;
 export const Navigation = styled.ul`
-width: 50vw;
-display: flex;
-align-items: center;
-justify-content: space-around;
-list-style: none;
+  width: 50vw;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  list-style: none;
 `;
 export const NavigationMenu = styled.li`
-display: flex;
-color: white;
-font-size: 20px;
-cursor: pointer;
-&:hover {
+  display: flex;
+  color: white;
+  font-size: 20px;
+  cursor: pointer;
+  &:hover {
     color: #ffc8a3;
     font-size: 20px;
-}
+  }
 `;

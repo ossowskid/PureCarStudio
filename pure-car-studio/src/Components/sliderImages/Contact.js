9c922eac5ec331787cdad158c58f866b58a4416c
ossowskid <ossowski.dawid@gmail.com>
2022-01-06 20:48:59 +0100

    something is not working. will fix it tomorrow

--- pure-car-studio/src/Components/sliderImages/Contact.js
+++ pure-car-studio/src/Components/sliderImages/Contact.js
@@ -1,10 +1,82 @@
-import { ContactSection } from "../../Styled/contact.styled";
+import Box from "@mui/material/Box";
+import { FormControl, InputLabel, Input } from "@mui/material";
+import {
+  ContactSection,
+  ContactMain,
+  ContactForm,
+  InformationBox,
+  ButtonSendEmail,
+} from "../../Styled/contact.styled";
 
 export const Contact = () => {
   return (
     <>
       <ContactSection id="contact">
-        <h2>Formularz kontaktowy</h2>
+        <p className="menu__category">Kontakt</p>
+        <ContactMain>
+          <ContactForm
+            action="https://formsubmit.co/ossowski.dawid@gmail.com"
+            method="POST"
+          >
+            <Box
+              component="form"
+              sx={{
+                "& > :not(style)": { m: 5, gap: 2 },
+              }}
+              noValidate
+              autoComplete="off"
+            >
+              <FormControl>
+                <input
+                  type="hidden"
+                  name="_next"
+                  value="http://localhost:3000"
+                />
+                <InputLabel
+                  htmlFor="component-outlinded"
+                  type="email"
+                  name="email"
+                  required
+                >
+                  Adres E-mail
+                </InputLabel>
+
+                <Input
+                  id="outlined-textarea"
+                  label="Adres Email"
+                  type="email"
+                  name="email"
+                  color="primary"
+                />
+                <FormControl>
+                  <InputLabel htmlFor="component-outlinded" required>
+                    Temat
+                  </InputLabel>
+                  <Input
+                    id="outlined-textarea"
+                    label="Temat"
+                    name="topic"
+                    type="text"
+                  />
+                </FormControl>
+                <FormControl>
+                  <InputLabel htmlFor="component-outlinded" required>
+                    Wiadomość...
+                  </InputLabel>
+                  <Input
+                    id="outlined-textarea"
+                    label="Treść wiadomośći"
+                    name="message"
+                    rows={10}
+                    multiline
+                  />
+                </FormControl>
+              </FormControl>
+            </Box>
+            <ButtonSendEmail type="submit">Send</ButtonSendEmail>
+          </ContactForm>
+          <InformationBox>Jakieś info</InformationBox>
+        </ContactMain>
       </ContactSection>
     </>
   );
--- pure-car-studio/src/Styled/contact.styled.js
+++ pure-car-studio/src/Styled/contact.styled.js
@@ -2,6 +2,36 @@ import styled from "styled-components";
 
 export const ContactSection = styled.div`
   display: flex;
+  flex-flow: column;
+  justify-content: center;
+  align-items: center;
   height: 100vh;
-  background-color: tomato;
+`;
+export const ContactMain = styled.div`
+  display: flex;
+  max-width: 80vw;
+  height: 70vh;
+`;
+export const ContactForm = styled.form`
+  display: flex;
+  flex-flow: column;
+  text-align: center;
+  justify-content: center;
+  background-color: rgba(227, 176, 140, 0.8);
+  height: 100%;
+  min-width: 50vw;
+`;
+
+export const InformationBox = styled.div`
+  display: flex;
+  width: 50%;
+  height: 100%;
+  background-color: red;
+`;
+
+export const ButtonSendEmail = styled.button`
+  background-color: white;
+  width: 150px;
+  height: 50px;
+  border: 1px solid #e3b08c;
 `;
